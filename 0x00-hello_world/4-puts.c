38fa4f8bbe013fbc6434ae1fcf90ec4a9d0c3ddd
lonercode <penpalclassmate@gmail.com>
2023-02-16 09:28:24 -0800

    C operations

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - The point of entry
+ * main - entry into code 
  *
  * Return: success(0)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

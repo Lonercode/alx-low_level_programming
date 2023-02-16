6d60f6ab470a3202c911ff695ddd6e2d8a8fc856
lonercode <penpalclassmate@gmail.com>
2023-02-16 10:12:30 -0800

    C operations

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(fd,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
+	return (1);
+}

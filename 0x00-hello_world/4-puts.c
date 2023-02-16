5767fc5a8667c03c7d4efbe51dee30a67816e381
lonercode <penpalclassmate@gmail.com>
2023-02-16 07:14:24 -0800

    C operations

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
-	fputs("\"programming is like building a multilingual puzzle\n");
+	puts(stderr,"Anything", "\"programming is like building a multilingual puzzle\n");
 	return 0;
 }

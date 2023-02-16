ac9f471b32586b9af027994282910d111cfe9853
lonercode <penpalclassmate@gmail.com>
2023-02-16 09:45:39 -0800

    C operations

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(){
-	printf("with proper grammer, but the outcome is a piece of art");
-	return 0;
+/**
+ * main - entry point 
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }

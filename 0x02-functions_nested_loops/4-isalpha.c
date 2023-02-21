e0341b585fdfebe4c58bb21917afde3867233444
lonercode <penpalclassmate@gmail.com>
2023-02-21 05:52:16 -0800

    created file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Determine if an alphabet
+ *@c: input
+ * Return: 0 or 1 (Depending on result)
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

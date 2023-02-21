32a6c4fd7ba71e7276b13cf7ee6e7bad6845c3ec
lonercode <penpalclassmate@gmail.com>
2023-02-21 04:57:30 -0800

    created file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabets 10 times
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void){
+char c;
+int i = 0;
+while (i < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar (c);
+}
+i++;
+_putchar("\n");
+}
+}

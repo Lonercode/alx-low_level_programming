8ad723dded01dc94408975b0d772dc879a0065d1
lonercode <penpalclassmate@gmail.com>
2023-02-21 10:05:24 -0800

    tired

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,32 @@
  */
 void print_to_98(int n)
 {
-if(n < 98){
-printf("%d, ", n);
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
 n++;
 }
-else if (n == 98)
-{
-printf("%d, ", n);
 }
-else
+else if (n > 98)
 {
-printf ("%d, ", n)
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
 n--;
 }
 }
+else
+{
+printf("98");
+}
+printf("\n");

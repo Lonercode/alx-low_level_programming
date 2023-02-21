ebd6d43fa84a2f9ac19183ca7da3c682dae49991
lonercode <penpalclassmate@gmail.com>
2023-02-21 12:47:52 -0800

    create file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Fibonacci Sequence
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+int i;
+int x = 1;
+int y= 2;
+int n = x + y;
+printf("Fibonacci Series: %d, %d, ", x, y);
+for (i = 3; i <= 50; ++i)
+{
+printf("%d, ", n);
+x = y;
+y = n;
+n = x + y;
+}
+return (0);
+}

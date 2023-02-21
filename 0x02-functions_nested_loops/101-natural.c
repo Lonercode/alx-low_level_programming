853cf27db948911fb10e7b4dd3f91bbf3971d438
lonercode <penpalclassmate@gmail.com>
2023-02-21 12:29:55 -0800

    fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,10 +9,12 @@ int main(void)
 {
 int n;
 int sum;
-for (n = 0; n < 1024; n++){
-if ((n % 5 ) == 0 || (n % 3) == 0){
-sum += n;      
-}   
+for (n = 0; n < 1024; n++)
+{
+if ((n % 5 ) == 0 || (n % 3) == 0)
+{
+sum += n;
+}
 }
 printf("%d\n", sum);
 return (0);

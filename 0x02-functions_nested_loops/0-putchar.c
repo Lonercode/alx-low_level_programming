24ec645eea238d8b9e29467511bbb25ffb6f0642
lonercode <penpalclassmate@gmail.com>
2023-02-21 03:55:40 -0800

    Checking for instruction

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ * main - Entry
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+    char c[] = "_putchar";
+    int i=0;
+    while (i<9)
+    {
+        _putchar(c[i]);
+        i++;
+    }
+    _putchar('\n');
+    return (0);
+}

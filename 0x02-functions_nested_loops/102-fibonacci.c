b82eb0c7bba2129bc3fef232d15d93fc3bb5a7a7
lonercode <penpalclassmate@gmail.com>
2023-02-21 13:07:51 -0800

    fixed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,8 +11,8 @@ int i;
 int x = 1;
 int y = 2;
 int n = x + y;
-printf("Fibonacci Series: %d, %d, ", x, y);
-for (i = 3; i <= 49; i++)
+printf("%d, %d, ", x, y);
+for (i = 3; i <= 50; ++i)
 {
 printf("%d, ", n);
 x = y;

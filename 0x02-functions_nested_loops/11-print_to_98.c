1b838a5190d17bce16bf742464fad7c69c24bf3d
lonercode <penpalclassmate@gmail.com>
2023-02-21 09:41:51 -0800

    file corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,15 @@ void print_to_98(int n)
 while(n < 98)
 {
 _putchar(n);
-_putchar(",");
-_putchar(" ");
+_putchar(',');
+_putchar(' ');
 n++;
 }
 while(n > 98)
 {
 _putchar(n);
-_putchar(",");
-_putchar(" ");
+_putchar(',');
+_putchar(' ');
 n--;
 }
 while(n == 98)

948da814d60604dc3a5c90f8cd600d89fe1e19f7
lonercode <penpalclassmate@gmail.com>
2023-02-21 05:26:50 -0800

    created file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Determine if lowercase
+ *
+ * Return: 0 or 1 (Depending on result)
+ */
+int _islower(int c)
+{
+int c;
+if (c > 91 && c < 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

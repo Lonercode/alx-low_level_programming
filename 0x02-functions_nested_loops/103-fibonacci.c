baeb086b28a75d750049c0a714ed4f3e337257b2
lonercode <penpalclassmate@gmail.com>
2023-02-21 14:08:18 -0800

    created file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
   * main - Fibonacci 2.
-  * 
+  *
   * Return: Nothing
   */
 int main(void)
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+  * main - first 98 of the  fibonacci series
+  * 
+  * Return: Nothing
+  */
+int main(void)
+{
+int x;
+unsigned long i, j, k;
+unsigned long m, n, l, val;
+x = 0;
+i = 0;
+j = 1;
+for (x = 1; x <= 91; x++)
+{
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
+}
+m = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (x <= 98)
+{
+val = (m + n) / 1000;
+l = (m + n) - val * 1000;
+k = (i + j) + val;
+m = n;
+n = l;
+i = j;
+j = k;
+if (l >= 100)
+printf("%lu%lu", k, l);
+else
+printf("%lu0%lu", k, l);
+if (x != 98)
+printf(", ");
+x++;
+}
+putchar('\n');
+return (0);
+}
+

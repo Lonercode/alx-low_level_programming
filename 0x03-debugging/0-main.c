e669efbb8ee8ac1e8523f01d68fe1c28b6f37e3d
lonercode <penpalclassmate@gmail.com>
2023-02-22 03:00:48 -0800

    Corrected output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
+* @i:input
 * Return: 0
 */
 
@@ -9,7 +10,7 @@ int main(void)
 {
         int i;
 
-        i = 0 ;
+        i = 0;
         positive_or_negative(i);
 
         return (0);

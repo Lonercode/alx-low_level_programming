655a6f8d3a885abeb2ccb6eefe7ac7e9beaaa89f
lonercode <penpalclassmate@gmail.com>
2023-02-23 07:26:20 -0800

    Corrected

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * print_most_numbers - Print numbers 0 - 9 except 2 and 4
- *@i:input
+ *
  * Return: Nothing
  */
 void print_most_numbers(void)
 {
 int i;
-for (i = 0; i <=9; i++)
+for (i = '0'; i <= '9'; i++)
 {
-if (i == 2 || i == 4)
+if (i == '2' || i == '4')
 {
 continue;
 }
 _putchar(i);
 }
-_putchar("\n");
+_putchar('\n');
 }

ce29bc3c5c625a310e09246de9c80a1decf56186
lonercode <penpalclassmate@gmail.com>
2023-02-23 08:36:47 -0800

    Corrected

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,26 +6,33 @@
  */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if ((i % 3 == 0) && (i % 5 != 0)){
-printf("Fizz ");
+	int x;
+
+	x = 0;
+	while (x < 100)
+	{
+		x++;
+		if ((x % 3) != 0 && (x % 5) != 0)
+		{
+			printf("%d", x);
+		}
+		else if ((x % 3)  == 0 && (x % 5) != 0)
+		{
+			printf("Fizz");
+		}
+		else if ((x % 3) != 0 && (x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("FizzBuzz");
+		}
+		if (x < 100)
+			putchar(32);
+	}
+	putchar('\n');
+	return (0);
 }
-else if ((i % 5 == 0) && (i % 3 != 0))
-{
-printf("Buzz ");
-}
-else if ((i % 5 == 0) && (i % 3 == 0))
-{
-printf("FizzBuzz ");
-}
-else 
-{
-printf("%d ", i);
-}
-}
-printf("\n");
-return (0);
-}
+
 

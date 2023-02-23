bf5ad0afb753faf2bd20e27966044b3536dbb121
lonercode <penpalclassmate@gmail.com>
2023-02-23 07:49:00 -0800

    Corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,26 @@
 #include "main.h"
 /**
- * more_numbers - Prints the numbers 1 - 14 ten times followed by a new line
- *
- * Return: Nothing
+ * more_numbers - Print numbers 1-14 ten times
+ * Return: nothing
  */
 void more_numbers(void)
 {
-int i,j = 0;
-while (j <= 9)
-{
-while (i <= 14)
-{
-if (i > 9)
- 	_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-i++;
-}
-_putchar('\n');
-j++;
-i = 0;
-}
+
+	int i = 0;
+	int j = 0;
+
+	while (j <= 9)
+	{
+		while (i <= 14)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(j % 10 + '0');
+			i++;
+
+		}
+	_putchar('\n');
+	j++;
+	i = 0;
+	}
 }

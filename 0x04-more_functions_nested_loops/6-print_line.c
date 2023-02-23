41fd1658d599b9400bf197216d920f4983542625
lonercode <penpalclassmate@gmail.com>
2023-02-23 08:07:47 -0800

    Corrected

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * print_line - Print a line whose length depends on the variable n
- *@n:input
+ * print_line - prints a line
+ * @n: input
  * Return: Nothing
  */
-int print_line(int n)
+void print_line(int n)
 {
-int i;
-if (n > 0)
-{
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
-else
-{
-_putchar('\n');
-}
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
 }

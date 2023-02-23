7a55e3f07460b91c72a66fb0916500658722daf5
lonercode <penpalclassmate@gmail.com>
2023-02-23 08:10:31 -0800

    Corrected

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,10 @@
  */
 void print_line(int n)
 {
-	int i = 0;
-
-	if (n > 0)
+	while (!(n <= 0))
 	{
-		while (i <= n)
-		{
-			_putchar('_');
-			i++;
-		}
-	 _putchar('\n');
-	}
-	else
+		_putchar('_');
+		n--;
+}
 	_putchar('\n');
 }

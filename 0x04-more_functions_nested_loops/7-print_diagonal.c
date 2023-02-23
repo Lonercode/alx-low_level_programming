61f57d2e5c7c36f8b23307642b657e01c0d2f856
lonercode <penpalclassmate@gmail.com>
2023-02-23 08:16:47 -0800

    Corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,22 @@
  */
 void print_diagonal(int n)
 {
-int i;
-int j;
-if (n > 0)
-{
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-_putchar('\\');
-else
-_putchar(' ');
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
-}
+int j, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		j = i;
+		while (j > 0)
+		{
+			_putchar(32);
+			j--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}

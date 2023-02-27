e0d96a8414fa8831b7550e2ef523ce0ae71a5934
lonercode <penpalclassmate@gmail.com>
2023-02-27 07:45:13 -0800

    file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,6 +8,44 @@
  */
 int main(void)
 {
-printf("Tada! Congrats");
+int i = 0;
+int r = 0;
+int n = 10;
+char nums[] = "0123456789";
+char lower[] = "abcdefghijklmnopqrstuvwxyz";
+char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char syms[] = "!@#$^&*()";
+char password;
+
+srand((unsigned int)(time(NULL)));
+r = rand() % 4;
+for ( i = 0; i < n; i++)
+{       
+if (r == 1)
+{
+password = nums[rand() % 10];
+r = rand() % 4;
+printf("%c", password);
+}
+else if (r == 2)
+{
+password = lower[rand() % 26];
+r = rand() % 4;
+printf("%c", password);
+}
+else if (r == 3)
+{
+password = upper[rand() % 26];
+r = rand() % 4;
+printf("%c", password);
+}
+else 
+{
+password = syms[rand() % 10];
+r = rand() % 4;
+printf("%c", password);
+}
+}
 return (0);
 }
+             

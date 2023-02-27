4653c515289d248dbccb0d62c349258c88f3238f
lonercode <penpalclassmate@gmail.com>
2023-02-27 01:18:36 -0800

    file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,20 @@
  */
 void rev_string(char *s)
 {
-print_rev(s);
+int i = 0;
+int j;
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < i; j++)
+{
+_putchar(s[j];)
+}
+_putchar('\n');
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
 }

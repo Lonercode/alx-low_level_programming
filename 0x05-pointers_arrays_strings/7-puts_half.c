f444f5adf3d92fb8e18b64209d554a8efd931205
lonercode <penpalclassmate@gmail.com>
2023-02-27 03:37:37 -0800

    file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - Print second half of a string
+ * @str: Input
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int j;
+int n;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+for (j = (i / 2); j < i; j++)
+{
+_putchar(str[j]);
+}
+}
+else if (i % 2 != 0)
+{
+for (n = (i - 1) / 2; n < i; n++)
+{
+_putchar(str[n]);
+}
+}
+_putchar('\n');
+}

c77a30e39c561d9610af64284a3328cdf8acdd99
lonercode <penpalclassmate@gmail.com>
2023-02-27 00:09:50 -0800

    file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Print string length
+ * @s: input
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		{
+			i++
+		}
+return (i);
+}

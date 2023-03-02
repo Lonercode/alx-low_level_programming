01f15e7a3836cde2faf908511b19f305f3eb31ee
lonercode <penpalclassmate@gmail.com>
2023-03-02 00:39:00 -0800

    file here

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,21 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
-    int j = 0;
-
-    while (dest[j] != '\0')
-    {
-        j++;
-    }
+int i;
     
-    for (i = 0; src[i] != '\0' && i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    for ( ; i < n; i++)
-	{
-	dest[i] = '\0';
-	}
-    return (dest);
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+for (i = 0; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }

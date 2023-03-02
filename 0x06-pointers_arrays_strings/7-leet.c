584c1d8338d3f320084b33ebac51f51650c64b17
lonercode <penpalclassmate@gmail.com>
2023-03-02 07:52:26 -0800

    file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,14 @@ char up[] = {'A', 'E', 'O', 'T', 'L'};
 char lo[] = {'a', 'e', 'o', 't', 'l'};
 char nu[] = {'4', '3', '0', '7', '1'};
 
-while (str[i] != 0)
+for (i = 0; str[i] != '\0'; i++)
 {
-i++;
+for (j = 0; i < 5; j++)
+{
+if (str[i] == up[j] || str[i] == lo[j])
+{
+str[i] = nu[j];
 }
-for (j = 0; j <= i; j++)
-{
-if (str[j] == up[j]  || str[j] == lo[j])
-{
-up[j] = lo[j] = nu[j];
-str[j] = nu[j];
 }
 }
 return (str);

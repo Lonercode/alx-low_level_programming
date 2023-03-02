447c41350d678fbf084e085d72b7bb58d553024c
lonercode <penpalclassmate@gmail.com>
2023-03-02 00:04:33 -0800

    file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,36 +3,22 @@
  * _strncat - Concatenates two strings with n
  * @dest: input 1
  * @src: input 2
- * Return: Result of concatenation 
+ * Return: Result of concatenation
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
-    int l;
- 
+int i = 0;
+int j = 0;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        j++;
-        i++;
-    }
-    dest[i] = '\0'
-    for (l = 0; l < n; l++)
-    {
-    if (l >= j)
-    {
-        return (src[j]);
-    }
-    else
-    {
-        return (src[l]);
-    }
-    }
-    return (0);
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
 }

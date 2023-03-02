70e457f75fd7fb0175b2d023112aa70e98747429
lonercode <penpalclassmate@gmail.com>
2023-03-02 08:31:10 -0800

    hey

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * ap_string - Capitalizes a string
- * @str: input 1
+ *cap_string - Capitalizes a string
+ *@str: input 1
  *
- * Return: Result
+ *Return: Result
  */
 char *cap_string(char *str)
 {

cec6eb68819f9560fe20750c51abf083e138ad15
lonercode <penpalclassmate@gmail.com>
2023-03-02 03:21:42 -0800

    file added

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,20 +6,19 @@
  */
 char *string_toupper(char *str)
 {
-    int i = 0;
-    int j;
+int i = 0;
+int j;
 
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-
-    for (j = 0; j < i; j++)
-    {
-        if ((str[j] >= 97) && (str[j] <= 122))
-        {
-            str[j] = str[j] - 32;
-        }
-    }
-    return (str);
+while (str[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < i; j++)
+{
+if ((str[j] >= 97) && (str[j] <= 122))
+{
+str[j] = str[j] - 32;
+}
+}
+return (str);
 }

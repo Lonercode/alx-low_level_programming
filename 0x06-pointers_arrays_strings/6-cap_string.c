863b0e40f49d30b0b933717198bf4b0a44036179
lonercode <penpalclassmate@gmail.com>
2023-03-02 08:15:32 -0800

    file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,4 +6,31 @@
  */
 char *cap_string(char *str)
 {
-"Expect the best. Pr
+int i;
+int j;
+int s[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+i = 0;
+while (*(str + i) != '\0')
+{
+if (*(str + i) >= 'a' && *(str + i) <= 'z')
+{
+if (i == 0)
+{
+*(str + i) = *(str + i) - 32;
+}
+else
+{
+for (j = 0; j <= 12; j++)
+{
+if (s[j] == *(str + i - 1))
+{
+*(str + i) = *(str + i) - 32;
+}
+}
+}
+}
+i++;
+}
+return (str);
+}

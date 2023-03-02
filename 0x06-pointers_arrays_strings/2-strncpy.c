fe5663b92d36ceb1a5cd981cc04e89d77dbc3984
lonercode <penpalclassmate@gmail.com>
2023-03-02 00:27:24 -0800

    file here

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,12 +6,13 @@
  * @n: input 3
  * Return: Result
  */
-char _strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
     int i;
     int j = 0;
 
-    while (dest[j] != '\0'){
+    while (dest[j] != '\0')
+    {
         j++;
     }
     

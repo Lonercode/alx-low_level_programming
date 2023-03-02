72c3bb2d7ce29c510d89bbaf632c5a66751a2181
lonercode <penpalclassmate@gmail.com>
2023-03-02 03:13:57 -0800

    file added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * string_toupper - Lowercase to upper
+ * @str: input 1
+ *
+ * Return: Result 
+ */
+char *string_toupper(char *str)
+{
+    int i = 0;
+    int j;
+
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+
+    for (j = 0; j < i; j++)
+    {
+        if ((str[j] >= 97) && (str[j] <= 122))
+        {
+            str[j] = str[j] - 32;
+        }
+    }
+    for (j = 0; j < i; j++)
+    {
+    _putchar(str[j]);
+    }
+}

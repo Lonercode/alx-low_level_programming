5012a40c2b38c4f00fba2667a8b7927d5aa23b76
lonercode <penpalclassmate@gmail.com>
2023-03-06 09:58:38 -0800

    file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * set_string - Sets the value of a pointer to char
+  * @s: input
+  * @to: input
+  *
+  * Return: Nothing.
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+

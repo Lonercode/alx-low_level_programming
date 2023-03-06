048fddee360922607d339af642c6d5b855748ef8
lonercode <penpalclassmate@gmail.com>
2023-03-06 05:32:57 -0800

    file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,30 @@
 #include "main.h"
+
 /**
- * _strchr - Find a character in a string
- * @s: input
- * @c: input
- * Return: Result
- */
+  * _strchr - Find character in string
+  * @s: input
+  * @c: input
+  *
+  * Return: Result
+  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-int j;
+	int i = 0
+	int j;
 
-while (s[i] != '\0')
-{
-    i++;
-}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-for (j = 0; j < i; j++)
-{
-    if (s[j] == c)
-    {
-        return (c);
-    }
-}
+	for (j = 0; j <= i; j++)
+	{
+		if (c == s[i])
+		{
+			s += j;
+			return (s);
+		}
+	}
+
 	return ('\0');
 }

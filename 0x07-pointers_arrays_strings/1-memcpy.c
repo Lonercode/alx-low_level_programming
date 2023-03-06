e7a35e060c0ae8212e3ba89786f7c7e53c08b5b0
lonercode <penpalclassmate@gmail.com>
2023-03-06 03:53:35 -0800

    file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - Copy string
+ * @dest:input
+ * @src:input
+ * @n:input
+ * Return:Result
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int j;
+
+for (j = 0; j < n; j++)
+{
+dest[j] = src[j];
+}
+return(dest);
+}

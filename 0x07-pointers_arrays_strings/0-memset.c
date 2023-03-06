2ceb39317fea0fea0cef77a6293bb5adc9d1cc63
lonercode <penpalclassmate@gmail.com>
2023-03-06 03:18:57 -0800

    file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,15 +9,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
-    int j;
+unsigned int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            s[j] = b;
-        }
-    }
-    return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }

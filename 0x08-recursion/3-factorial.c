064c5e114f159ea48963cae18f6d3a5e68abd1be
lonercode <penpalclassmate@gmail.com>
2023-03-08 04:25:12 -0800

    file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,29 +1,16 @@
 #include "main.h"
 /**
- * factorial - Print string with recusrion
- * @n: input
- * 
- * Return: Factorial of number.
- */
-long int fact(int n);
-
-int factorial(int n)
-{
-return (fact(n));
-}
-
-/**
- * fact - Factorial with recursion
+ * factorial - Factorial with recursion
  * @n: input
  * 
  * Return: Factorial of Number
  */
 
-long int fact(int n)
+int factorial(int n)
 {
 if (n >= 0)
 {
-return(n * fact(n - 1));
+return(n * factorial(n - 1));
 }
 else
 {

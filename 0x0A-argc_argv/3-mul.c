991a5aa04d5d6b977f0ea37d8297c0d9966cbfb1
lonercode <penpalclassmate@gmail.com>
2023-03-10 09:13:43 -0800

    file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,19 @@
 int main(int argc, char *argv[])
 {
     int args = 2;
+    int mult;
+    int i;
     
     if (argc == args)
     {
-        int n1 = atoi(argv[1]);
-        int n2 = atoi(argv[2]);
-        int mult = n1 * n2;
-        printf("%d\n", mult);
+        mult = 1;
+
+		for (i = 1; i < 3; i++)
+        {
+			mult *= atoi(argv[i]);
+        }
+
+		printf("%d\n", mult);
     }
     else
     {

e1e5437b3a52b709d43a24099f77ca07b64414a2
lonercode <penpalclassmate@gmail.com>
2023-03-10 06:12:38 -0800

    file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Name of program
+ * @argc: input
+ * @argv: input
+ * Return: Something
+ */
+
+int main(int argc, char *argv[])
+{
+    printf(argv[0]);
+    putchar('\n');
+
+    return (0);
+}

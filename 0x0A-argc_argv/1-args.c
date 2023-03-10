020fd9140c3e7949f33518b48916d6d33e38eec5
lonercode <penpalclassmate@gmail.com>
2023-03-10 06:19:58 -0800

    file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Number of arguments for program
+ * @argc: input
+ * @argv: input
+ * Return: Something
+ */
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n",argc);
+
+    return (0);
+}

3cce24fe92084baddfe9ec60f1ebe7c7cea849c6
lonercode <penpalclassmate@gmail.com>
2023-03-10 11:43:40 -0800

    file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,18 +11,17 @@ int change(int i);
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("%s\n", "Error");
-		return (1);
-	}
-	else if (argc < 0)
-	{
-		return (0);
-	}
-
-	printf("%d\n", change(atoi(argv[1])));
-	return (0);
+if (argc != 2)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else if (argc < 0)
+{
+return (0);
+}
+printf("%d\n", change(atoi(argv[1])));
+return (0);
 }
 
 /**
@@ -32,36 +31,36 @@ int main(int argc, char *argv[])
  */
 int change(int i)
 {
-	int q = 25, d = 10, n = 5, t = 2, p = 1;
-	int c;
+int q = 25, d = 10, n = 5, t = 2, p = 1;
+int c;
 
-	while (i > 0)
-	{
-		while (i >= q)
-		{
-			i -= q;
-			c++;
-		}
-		while (i >= d)
-		{
-			i -= d;
-			c++;
-		}
-		while (i >= n)
-		{
-			i -= n;
-			c++;
-		}
-		while (i >= t)
-		{
-			i -= t;
-			c++;
-		}
-		while (i >= p)
-		{
-			i -= p;
-			c++;
-		}
-	}
-	return (c);
+while (i > 0)
+{
+while (i >= q)
+{
+i -= q;
+c++;
+}
+while (i >= d)
+{
+i -= d;
+c++;
+}
+while (i >= n)
+{
+i -= n;
+c++;
+}
+while (i >= t)
+{
+i -= t;
+c++;
+}
+while (i >= p)
+{
+i -= p;
+c++;
+}
+}
+return (c);
 }

c9c6b5792f7655226fb2d4eaeccb7f74a7ec66b0
lonercode <penpalclassmate@gmail.com>
2023-03-10 09:01:56 -0800

    file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Multiply two numbers
+ * @argc: input
+ * @argv: input
+ * Return: Something
+ */
+
+int main(int argc, char *argv[])
+{
+    int args = 2;
+    
+    if (argc == args)
+    {
+        int n1 = atoi(argv[1]);
+        int n2 = atoi(argv[2]);
+        int mult = n1 * n2;
+        printf("%d\n", mult);
+    }
+    else
+    {
+        printf("Error");
+        putchar('\n');
+        return (1);
+    }
+    
+return(0);
+    
+}

e9d2af183f8ba5fbbb971a69244c812ad8360428
lonercode <penpalclassmate@gmail.com>
2023-03-10 12:02:31 -0800

    file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,67 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- *main - Entry to code
- *@argc: input
- *@argv: input
- *@i: input
- *Return: 0
- */
 int change(int i);
-
+/**
+ * main - Entry to code
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-if (argc != 2)
-{
-printf("%s\n", "Error");
-return (1);
-}
-else if (argc < 0)
-{
-return (0);
-}
-printf("%d\n", change(atoi(argv[1])));
-return (0);
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else if (argc < 0)
+	{
+		return (0);
+	}
+
+	printf("%d\n", change(atoi(argv[1])));
+	return (0);
 }
 
 /**
- *change - change
- *@i: input
- *Return: change
+ * change - change
+ * @i: input
+ * Return: change
  */
 int change(int i)
 {
-int q = 25, d = 10, n = 5, t = 2, p = 1;
-int c;
+	int q = 25, d = 10, n = 5, t = 2, p = 1;
+	int c;
 
-while (i > 0)
-{
-while (i >= q)
-{
-i -= q;
-c++;
+	while (i > 0)
+	{
+		while (i >= q)
+		{
+			i -= q;
+			c++;
+		}
+		while (i >= d)
+		{
+			i -= d;
+			c++;
+		}
+		while (i >= n)
+		{
+			i -= n;
+			c++;
+		}
+		while (i >= t)
+		{
+			i -= t;
+			c++;
+		}
+		while (i >= p)
+		{
+			i -= p;
+			c++;
+		}
+	}
+	return (c);
 }
-while (i >= d)
-{
-i -= d;
-c++;
-}
-while (i >= n)
-{
-i -= n;
-c++;
-}
-while (i >= t)
-{
-i -= t;
-c++;
-}
-while (i >= p)
-{
-i -= p;
-c++;
-}
-}
-return (c);
-}
+

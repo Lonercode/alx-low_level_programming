ea23694eb9897bd04bda9287c13a41615207a00f
lonercode <penpalclassmate@gmail.com>
2023-03-13 14:44:01 -0700

    files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 /**
- * str_concat - str_concat
+ * str_concat - concat
  * @s1: input
  * @s2: input
  * Return: Result
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *str;
+	int a, b, c;
+	char *my_array;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	c = 0;
+	my_array = malloc(sizeof(*s1 + *s2));
 
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
-
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
-
-	str = malloc(sizeof(char) * (end1 + end2 + 1));
-
-	if (str == NULL)
+	if (my_array == NULL)
 		return (NULL);
 
-	while (*s1)
+	a = 0;
+	while (s1[a] != '\0')
 	{
-		str[i] = *s1;
-		i++;
-		s1++;
+		my_array[c] = s1[a];
+		a++;
+		c++;
+	}
+	b = 0;
+	while (s2[b] != '\0')
+	{
+		my_array[c] = s2[b];
+		b++;
+		c++;
 	}
 
-	while (*s2)
-	{
-		str[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (str);
+	return (my_array);
+	free(my_array);
 }

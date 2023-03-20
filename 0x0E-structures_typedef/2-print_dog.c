731ac47d28e8b61cd8eacf2757138f3d69496e04
lonercode <penpalclassmate@gmail.com>
2023-03-20 06:37:09 -0700

    file added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,28 +11,15 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(d->name))
-		{
-		printf("Name: (nil)\n");
-		}
+			printf("Name: (nil)\n");
 		else
-		{
-		printf("Name: %s\n", d->name);
-		}
-		if (!(d->age))
-		{
-		printf("Age: (nil)\n");
-		}
-		else
-		{
+			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
-		}
+
 		if (!(d->owner))
-		{
-		printf("Owner: (nil)\n");
-		}
+			printf("Owner: (nil)\n");
 		else
-		{
-		printf("Owner: %s\n", d->owner);
-		}
+			printf("Owner: %s\n", d->owner);
 	}
 }

0527a99150bb4028089dd9ee118745e9db1c6243
lonercode <penpalclassmate@gmail.com>
2023-03-22 06:10:46 -0700

    file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
-/** print_name - Print a name
- * @name: input
- * @f: function pointer
+/**
+ * print_name - print a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: Nothing
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {

1e4d7abb5285086af792de1e77ab4de5553121f5
lonercode <penpalclassmate@gmail.com>
2023-03-22 05:59:19 -0700

    file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,4 @@
 #include "function_pointers.h"
-/** f - Print a name
- * @name: input
- *
- * Return: Nothing
- */
-void f(char *name)
-{
-	int i = 0;
-        int j;
-        while (name[i] != '\0')
-        {
-                i++;
-        }
-        for (j = 0; j < i; j++)
-        {
-        _putchar("%c", name[j]);
-        }
-}
 
 /** print_name - Print a name
  * @name: input
@@ -26,7 +8,6 @@ void f(char *name)
  */
 void print_name(char *name, void (*f)(char *))
 {
-	void (*f)(char *) = &f;
-
-        (*f)(name);
+	if (name && f)
+		f(name);
 }

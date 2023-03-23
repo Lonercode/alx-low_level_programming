a02c6fa4848291aff00ae9ffe5c29584ddf40096
lonercode <penpalclassmate@gmail.com>
2023-03-23 14:48:45 -0700

    files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
  
 /**
@@ -7,26 +6,23 @@
  *
  * Return: varies
  */
-int sum_them_all(const insigned int n, ...)
+iint sum_them_all(const insigned int n, ...)
 {
-	if (n != 0)
-{
-    int sum = 0;
- 
-    va_list ptr;
- 
-    va_start(ptr, n);
- 
-    for (int i = 0; i < n; i++)
-	{
-        Sum += va_arg(ptr, int);
-	}
-    va_end(ptr);
- 
-    return (sum);
-}
 if (n == 0)
 {
-	return (0);
+return (0);
+}
+else
+{
+int sum = 0; 
+va_list ptr;
+va_start(ptr, n);
+ 
+for (int i = 0; i < n; i++)
+{
+sum += va_arg(ptr, int);
+}
+va_end(ptr);
+return (sum);
 }
 }

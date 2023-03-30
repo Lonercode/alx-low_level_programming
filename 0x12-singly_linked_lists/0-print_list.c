6dfa311e8c6d4dade0a834b106e16816b9293f9a
lonercode <penpalclassmate@gmail.com>
2023-03-30 06:49:44 -0700

    added files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - Prints all the elements of the list_t list
+ * @h: list pointer
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+	if (ptr->str != NULL)
+	{
+		printf("[%d] %s\n", ptr->len, ptr->str);
+	}
+	else
+	{
+		printf("[0] (nil)\n");
+		nodes++;
+		ptr = ptr->next;
+	}
+	}
+	return (nodes);
+}

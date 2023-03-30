d99901184ebdfa616ce01886d3ff7b54ef25ded0
lonercode <penpalclassmate@gmail.com>
2023-03-30 07:43:37 -0700

    files are added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node != NULL)
 	{
 		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
+		new_node->len = lens(str);
 		new_node->next = *head;
 	}
 	else
@@ -27,12 +27,12 @@ list_t *add_node(list_t **head, const char *str)
 }
 
 /**
- * _strlen - String length
+ * lens - String length
  * @str: string
  *
  * Return: string length
  */
-int _strlen(const char *str)
+int lens(const char *str)
 {
 	int i = 0;
 

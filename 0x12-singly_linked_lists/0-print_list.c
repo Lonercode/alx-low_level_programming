68b17e1d605f0101c0d9678625ed468db0e0112b
lonercode <penpalclassmate@gmail.com>
2023-03-30 07:03:07 -0700

    added files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,21 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-	const list_t *ptr = h;
+size_t nodes = 0;
+const list_t *ptr = h;
 
-	while (ptr != NULL)
-	{
-	if (ptr->str != NULL)
-	{
-		printf("[%d] %s\n", ptr->len, ptr->str);
-	}
-	else
-	{
-		printf("[0] (nil)\n");
-	}
-		nodes++;
-		ptr = ptr->next;
-	
-	}
-	return (nodes);
+while (ptr != NULL)
+{
+if (ptr->str != NULL)
+{
+printf("[%d] %s\n", ptr->len, ptr->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+nodes++;
+ptr = ptr->next;
+}
+return (nodes);
 }

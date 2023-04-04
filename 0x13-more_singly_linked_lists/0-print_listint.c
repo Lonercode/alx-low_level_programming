c8f9f333f6c6ba4ce20415e710039364dc2ca3ae
lonercode <penpalclassmate@gmail.com>
2023-04-04 14:26:55 -0700

    file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements
- * @h: head of the linked list
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *ptr = h;
-	size_t nodes = 0;
+const listint_t *cursor = h;
+size_t count = 0;
 
-	while (ptr != NULL)
-	{
-		printf("%d\n", ptr->n);
-		nodes++;
-		ptr = ptr->next;
-	}
-	return (nodes);
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
 }
-
+return (count);
+}

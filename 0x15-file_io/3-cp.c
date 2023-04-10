5d6c868a955f94420fecfd4db4b46730e8f012d4
lonercode <penpalclassmate@gmail.com>
2023-04-10 08:12:17 -0700

    f

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 		}
 
 		w = write(end, buffer, r);
-		if (to == -1 || w == -1)
+		if (end == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);

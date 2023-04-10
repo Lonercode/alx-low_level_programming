a01c804f4f7610c991397442e9d39d3a2951c31d
lonercode <penpalclassmate@gmail.com>
2023-04-10 08:25:57 -0700

    fck

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,44 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * read_textfile - Function that reads a file and prints it to POSIX Standard Output
- * @filename: input
- * @letters: input
- *
- * Return: 0 or number of bytes
+ * read_textfile - function that reads a text file and prints it to the POSIX standard output.
+ * @filename: Filename
+ * @letters: number of letters to be read/printed
+ * 
+ * Return: Number of letters read/printed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int i, j, fd;
-char *buf;
-if (!filename)
-	return (0);
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-	return (0);
-buffer = malloc(sizeof(char) * letters);
-if (!buffer)
-	return (0);
-i = read(fd, buffer, letters);
-if (i < 0)
+	ssize_t i;
+    ssize_t j;
+    ssize_t k;
+	char *b;
+
+if (filename == NULL)
 {
-free(buffer);
-return (0);
+	return (0);
 }
-buffer[i] = '\0';
-close(fd);
-j = write(STDOUT_FILENO, buffer, i);
-if (j < 0)
+else
 {
-free(buffer);
-return (0);
+    b = malloc(sizeof(char) * letters);
 }
-free(buffer);
-return (j);
+if (b == NULL)
+{
+	return (0);
+}
+else
+{
+	i = open(filename, O_RDONLY);
+	j = read(i, b, letters);
+	k = write(STDOUT_FILENO, b, j);
+}
+if (i == -1 || j == -1 || k == -1 || k != r)
+{
+	free(b);
+	return (0);
+}
+
+free(b);
+close(i);
+
+return (k);
 }

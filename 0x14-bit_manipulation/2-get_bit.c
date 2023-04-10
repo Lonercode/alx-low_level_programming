5afac172b4edb8af7d32148a7f85d49a02c71811
lonercode <penpalclassmate@gmail.com>
2023-04-10 13:39:10 -0700

    file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * get_bit - value of bit at index
+ * get_bit - bit value
  * @n: input
- * @index: input
- * 
- * Return: Value of bit at index or -1
+ * @index: index
+ * Return: bit value or -1
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int j;
+	unsigned long int x;
 
-if (index > 64)
-	return (-1);
+	if (index > 64)
+		return (-1);
 
-j = n >> index;
+	x = n >> index;
 
-return (j & 1);
+	return (x & 1);
 }

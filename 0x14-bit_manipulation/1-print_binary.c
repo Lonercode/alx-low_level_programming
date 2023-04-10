30e3e99a6bfe1da8511ea014952aff94189c4f15
lonercode <penpalclassmate@gmail.com>
2023-04-10 12:30:59 -0700

    files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,25 +7,25 @@
  *
  * Return: Nothing
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
-	int s;
+unsigned long int i;
+int s;
 
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	for (i = n, s = 0; (i >>= 1) > 0; s++);
-
-	for (; s >= 0; s--)
-	{
-		if ((n >> s) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+if (n == 0)
+{
+printf("0");
+return;
 }
 
+for (i = n, s = 0; (i >>= 1) > 0; s++);
+
+for (; s >= 0; s--)
+{
+if ((n >> s) & 1)
+printf("1");
+else
+printf("0");
+}
+}

caaa25f2a68461f189315650a7d3f1395a5e9e38
lonercode <penpalclassmate@gmail.com>
2023-04-10 06:02:19 -0700

    files here

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int i, k, l = 0;
+int i, j, l = 0;
 
 if (filename == NULL)
 {
-    return (-1);
+return (-1);
 }
 if (text_content != NULL)
 {
-    for (l = 0; text_content[l];)
-        l++;
-}
-else
+for (l = 0; text_content[l];)
 {
-i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-k = write(i, text_content, l);
+l++;
 }
-if (i == -1 || k == -1)
-    return (-1);
+}
 
+i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+j = write(i, text_content, l);
+
+if (i == -1 || j == -1)
+{
+return (-1);
+}
 close(i);
-
 return (1);
 }

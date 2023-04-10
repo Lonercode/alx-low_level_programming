d86e2a7b7600bbee2b7808658dbb9e9bd77fb243
lonercode <penpalclassmate@gmail.com>
2023-04-10 07:12:19 -0700

    filez

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,27 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t i, j, k;
-char *buff;
+	ssize_t i, j, k;
+	char *buff;
 
-if (filename == NULL)
-{
-return (0);
-}
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
-{
-return (0);
-}
-i = open(filename, O_RDONLY);
-j = read(i, buff, letters);
-k = write(STDOUT_FILENO, buff, j);
-if (i == -1 || j == -1 || k == -1 || k != j)
-{
-free(buff);
-return (0);
-}
-free(buff);
-close(i);
-return (k);
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	i = open(filename, O_RDONLY);
+	j = read(i, buffer, letters);
+	k = write(STDOUT_FILENO, buffer, r);
+
+	if (i == -1 || j == -1 || k == -1 || k != r)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(i);
+
+	return (k);
 }
